eafc41ca309302b32923e3b8cfe708c1222247fc
Shin <shinoku1801@gmail.com>
2022-12-02 12:06:43 +0700

    init project

---
+++ assets/js/config.js
@@ -0,0 +1,19 @@
+export function getBrowserWidth() {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth,
+    );
+}
+
+export function getBrowserHeight() {
+    return Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.documentElement.clientHeight,
+    );
+}
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+import { getBrowserWidth, getBrowserHeight } from './config.js';
+
+const browserWidth = getBrowserWidth();
+const browserHeight = getBrowserHeight();
+
+console.log(`Width: ${browserWidth}`);
+console.log(`Height: ${browserHeight}`);
