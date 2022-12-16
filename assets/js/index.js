9103cb936cb48759c74d7e5d05cbee5b9cb166e3
Shin <shinoku1801@gmail.com>
2022-12-16 23:10:09 +0700

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,1 @@
-import { getBrowserWidth, getBrowserHeight } from './config.js';
 
-const browserWidth = getBrowserWidth();
-const browserHeight = getBrowserHeight();
-
-console.log(`Width: ${browserWidth}`);
-console.log(`Height: ${browserHeight}`);
